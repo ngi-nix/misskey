27adcebcf28f82fa7881a4a2e9898741a3a3a6c6
Mihai Fufezan <fufexan@pm.me>
2021-10-14 18:56:48 +0300

    flake: add initial module

--- flake.nix
+++ flake.nix
@@ -83,5 +83,162 @@
           # TODO: add python3 and libc.dev
           buildInputs = with pkgs; [ yarn2nix yarn nodejs gcc libtool nasm pkgconfig zlib.dev git ];
         });
+
+      nixosModules = {
+        misskey = { lib, pkgs, config, ... }:
+          with lib;
+          let
+            cfg = config.services.misskey;
+            yaml = pkgs.formats.yaml { };
+            configFile =
+              yaml.generate "default.yml" cfg.settings;
+            # finalPackage is a variable which takes the user defined package for
+            # misskey, such as 'services.misskey.package = pkgs.myCustomPackage'
+            # and applies the user defined misskey config to it. This means
+            # that no matter what package the user provides, it will be processed
+            # and contain the default.yml (config file) in its output.
+            finalPackage = pkgs.runCommandNoCC "misskey-with-config" { } ''
+              cp -rs ${cfg.package} $out
+              chmod +w -R $out
+              ln -sf ${configFile} $out/.config/default.yml
+            '';
+            misskeyDefaultYml =
+              # misskeyDefaultYml is a variable which writes and merges the default
+              # config.json from the misskey package with the user defined config
+              # from the nixosModule. In the case of a collision, the user
+              # defined configuration takes precedence, since it is passed as the
+              # second argument in lib.recursiveUpdate.
+              pkgs.writeText "default.yml" (
+                builtins.toJSON (
+                  lib.recursiveUpdate
+                    (builtins.fromJSON
+                      (builtins.readFile "${pkgs.misskey}/.config/example.yml")
+                    )
+                    (builtins.fromJSON
+                      (builtins.readFile (yaml.generate "default.yml" cfg.settings))
+                    )
+                )
+              );
+          in
+          {
+            options.services.misskey = {
+              enable = mkEnableOption "Misskey";
+
+              dataDir = mkOption {
+                type = types.str;
+                default = "/var/lib/misskey";
+                description = ''
+                  The directory where Misskey stores its data files. If left as the default value this directory will automatically be created before the Misskey server starts, otherwise the sysadmin is responsible for ensuring the directory exists with appropriate ownership and permissions.
+                '';
+              };
+
+              package = mkOption {
+                type = types.pkg;
+                default = pkgs.misskey;
+                description = ''
+                  Package for Misskey.
+                '';
+              };
+
+              extraConfig = mkOption {
+                type = types.str;
+                default = ''
+                  arstneio
+                '';
+                description = "Misskey configuration.";
+              };
+
+              settings = mkOption {
+                type = with types; submodule { freeformType = yaml.type; };
+                description = "Misskey configuration, in submodule form.";
+              };
+
+              user = mkOption {
+                type = types.str;
+                default = "misskey";
+                description = "User account under which Misskey runs.";
+              };
+
+              group = mkOption {
+                type = types.str;
+                default = "misskey";
+                description = "Group under which Misskey runs.";
+              };
+            };
+
+            config = mkIf cfg.enable {
+              nixpkgs.overlays = [ self.overlay ];
+
+              #assertions = [
+              #  {
+              #    assertion = cfg.extraConfig != "" && cfg.settings != { };
+              #    message = "The option `extraOptions` conflicts with `settings`. Use only one of them";
+              #  }
+              #];
+
+              systemd.services.misskey = {
+                description = "Misskey social platform";
+                wantedBy = [ "multi-user.target" ];
+
+                serviceConfig = mkMerge [
+                  {
+                    User = cfg.user;
+                    Group = cfg.group;
+                    WorkingDirectory = cfg.dataDir;
+                    ExecStart = "${finalPackage}/bin/misskey";
+                    Restart = "on-failure";
+                  }
+                  (mkIf (cfg.dataDir == "/var/lib/misskey") { StateDirectory = "misskey"; })
+                ];
+              };
+
+              users.users = mkIf (cfg.user == "misskey") {
+                misskey = {
+                  isSystemUser = true;
+                  group = cfg.group;
+                  description = "Misskey system user";
+                };
+              };
+
+              users.groups = mkIf (cfg.group == "misskey") { misskey = { }; };
+            };
+          };
+      };
+
+      checks = forAllSystems (system:
+        with nixpkgsFor.${system};
+        lib.optionalAttrs stdenv.isLinux {
+          # A VM test of the NixOS module.
+          vmTest = with import (nixpkgs + "/nixos/lib/testing-python.nix")
+            {
+              inherit system;
+            };
+
+            let
+              test = makeTest {
+                nodes = {
+                  client = { config, pkgs, ... }: {
+                    environment.systemPackages = [ pkgs.curl ];
+                  };
+                  misskey = { config, pkgs, ... }: {
+                    imports = [ self.nixosModules.misskey ];
+                    services.misskey.enable = true;
+                    networking.firewall.allowedTCPPorts = [ 8000 ];
+                  };
+                };
+
+                testScript = ''
+                  start_all()
+                  misskey.wait_for_unit("misskey.service")
+                  misskey.wait_for_open_port("8000")
+                  client.wait_for_unit("multi-user.target")
+                  client.succeed("curl -sSf http:/misskey:8000/static/config.json")
+                  misskey.succeed("cat ${test.nodes.misskey.config.services.misskey.configFile} >&2")
+                  misskey.succeed("cat ${test.nodes.misskey.config.services.misskey.webircgateway.configFile} >&2")
+                '';
+              };
+            in
+            test;
+        });
     };
 }
